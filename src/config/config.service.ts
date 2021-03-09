0fe535d411a58292a7bd4bd999e4bab7545affb2
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-03-09 19:31:06 +0800

    添加配置

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -1,4 +1,51 @@
 import { Injectable } from '@nestjs/common';
 
+export const env = "test"
+
+export class Config {
+  mysql: {
+    host: string;
+    port: number;
+    username: string;
+    password: string;
+  };
+  redis: {
+    host: string;
+    port: number;
+    password: string;
+  }
+}
+
 @Injectable()
-export class ConfigService {}
+export class ConfigService {
+  static getConfig (): Config {
+    const config = new Config;
+    if (env === "test") {
+      config.mysql = {
+        host: "127.0.0.1",
+        port: 3306,
+        username: "root",
+        password: "OPENtextfile+123"
+      };
+      config.redis = {
+        host: "127.0.0.1",
+        port: 6379,
+        password: "OPENtextfile+123"
+      }
+    } else if (env === "dev") {
+      config.mysql = {
+        host: "127.0.0.1",
+        port: 3306,
+        username: process.env.MYSQL_USERNAME,
+        password: process.env.MYSQL_PASSWORD
+      };
+      config.redis = {
+        host: "127.0.0.1",
+        port: 16379,
+        password: process.env.REDIS_PASSWORD
+      }
+    }
+
+    return config;
+  }
+}
