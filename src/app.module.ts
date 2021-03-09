8ad49fab8087c20cf6ae194c7cff0e6235bd79dd
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-03-09 19:32:01 +0800

    init mysql and redis module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,11 @@ import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ConfigModule } from './config/config.module';
 import { ContestModule } from './contest/contest.module';
+import { MysqlModule } from './mysql/mysql.module';
+import { RedisModule } from './redis/redis.module';
 
 @Module({
-  imports: [UserModule, ConfigModule, ContestModule],
+  imports: [UserModule, ConfigModule, ContestModule, MysqlModule, RedisModule],
   controllers: [AppController],
   providers: [AppService],
 })
