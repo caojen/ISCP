da4412abde2aa279509b20d09e0420e4089abf54
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-03-11 15:55:53 +0800

    将/暴露出来

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { requestListening } from './util/request.middleware';
+import * as express from 'express';
 
 const PORT = 5003;
 
@@ -8,6 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(requestListening);
   app.setGlobalPrefix('/api');
+  app.use('/', express.static('static'));
   await app.listen(PORT);
 }
 bootstrap();
