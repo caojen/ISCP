08f35fcd98fa95238bbb771db707f4228f48b595
曹鉴恩 <caojen@mail2.sysu.edu.cn>
2021-03-13 11:43:36 +0800

    use static asset

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,16 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { requestListening } from './util/request.middleware';
 import * as express from 'express';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 const PORT = 5003;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   app.use(requestListening);
   app.setGlobalPrefix('/api');
-  app.use('/', express.static('static'));
+  // app.use('/*', express.static('static'));
+  app.useStaticAssets('../static')
   await app.listen(PORT);
 }
 bootstrap();
